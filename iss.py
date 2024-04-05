77a649f3ec91547c0be3eafabd07e62428b19e10
anhambombe <84500027+anhambombe@users.noreply.github.com>
2024-04-05 23:07:05 +0200

    Update iss.py

--- iss.py
+++ iss.py
@@ -123,7 +123,7 @@ with st.container():
 
 prov=st.sidebar.multiselect(
     "Provincia",
-    df["states"].unique(),df["states"].unique()[-2]
+    df["states"].unique(),df["states"].unique()[-6]
 )
 
 anos=st.sidebar.multiselect(
